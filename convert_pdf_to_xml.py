339485e6a9db74501a4311b75fd09d6c33b4f533
abhipoo <abhishek@letstranzact.com>
2020-05-10 14:35:33 +0530

    Added windows .exe to convert files

--- convert_pdf_to_xml.py
+++ convert_pdf_to_xml.py
@@ -1,24 +1,50 @@
 import pdfquery
 from lxml import etree
 import os
+import traceback
 
 def convert(filename, filepath = ""):
-    if filepath:
-        pdf = pdfquery.PDFQuery(filepath + "/" + filename)
-        pdf.load()
-        with open('{}/{}.xml'.format(filepath, filename.replace(".pdf", "")),'wb') as f:
-            f.write(etree.tostring(pdf.tree, pretty_print=True))
-    else:   
-        pdf = pdfquery.PDFQuery(filename)
-        pdf.load()
-        with open('{}.xml'.format(filename.replace(".pdf", "")),'wb') as f:
-            f.write(etree.tostring(pdf.tree, pretty_print=True))
-
+    try:
+        if filepath:
+            pdf = pdfquery.PDFQuery(filepath + "/" + filename)
+            pdf.load()
+            with open('{}/{}.xml'.format(filepath, filename.replace(".pdf", "")),'wb') as f:
+                f.write(etree.tostring(pdf.tree, pretty_print=True))
+        else:   
+            pdf = pdfquery.PDFQuery(filename)
+            pdf.load()
+            with open('{}.xml'.format(filename.replace(".pdf", "")),'wb') as f:
+                f.write(etree.tostring(pdf.tree, pretty_print=True))
+    except:
+        print(traceback.format_exc())
 
 if __name__ == "__main__":
-    directory = "resources/Sample files/PDFFiles"
-    files = os.listdir(directory)
-    #print(files)
-    #convert(files[0], directory)
-    for file in files:
-        convert(file, directory)
+    #directory = "resources/Sample files/PDFFiles"
+    directory = input("Enter directory path containing PDF files [default : Enter] : ")
+    print(directory)
+    
+    while(True):
+        bulk_flag = input("Convert all files in this directory (Y [default : Enter]/ N) ? : ").upper()
+        
+        if bulk_flag == "Y" or bulk_flag == "":
+            if directory:
+                files = os.listdir(directory)
+            else:
+                files = os.listdir()
+
+            for filename in files:
+                if filename.lower().endswith('.pdf'):
+                    print("Converting file :" + filename)
+                    convert(filename, directory)
+            break
+        elif bulk_flag == "N":
+            filename = input("Please enter PDF filename to be converted : ")
+
+            if filename.lower().endswith('.pdf'):
+                convert(filename, directory)
+            else:
+                convert(filename + ".pdf", directory)
+            break
+        else:
+            print("Please choose either Y [Enter] / N to continue..")
+
